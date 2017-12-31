aa4e3d636b44f3fae2b001920a38eabd5b1f2917
Alex Feyerke <espy@users.noreply.github.com>
2017-12-31 12:24:14 +0100

    fix: window dragging plus theme dropping

--- views/main.js
+++ views/main.js
@@ -11,9 +11,24 @@ function view (state, emit) {
   function onDrop (e) {
     emit('drop', e)
   }
+  function onDragOver (e) {
+    return false
+  }
+  function onDragLeave (e) {
+    return false
+  }
+  function onDragEnd (e) {
+    return false
+  }
 
   return html`
-    <body class="sans-serif" ondrop=${onDrop}>
+    <body
+      class="sans-serif"
+      ondrop=${onDrop}
+      ondragover=${onDragOver}
+      ondragleave=${onDragLeave}
+      ondragend=${onDragEnd}
+    >
       ${fileBrowser(state, emit)}
       ${state.params.date
         ? textPane.render(state, emit)
