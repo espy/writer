a98619fbcfd0687d33be46cc8561f5dc08a59e31
Alex Feyerke <espy@users.noreply.github.com>
2018-01-02 13:21:40 +0100

    wip: figured out correct electron resources paths for dev and prod

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const css = require('sheetify')
 const choo = require('choo')
+const {remote} = window.require('electron')
 
 css('tachyons')
 css('./assets/main.css')
@@ -7,6 +8,7 @@ css('./assets/main.css')
 var app = choo()
 if (process.env.NODE_ENV !== 'production') {
   app.use(require('choo-devtools')())
+  console.log('remote.process.env.NODE_ENV', remote.process.env.NODE_ENV)
 } else {
   // Enable once you want service workers support. At the moment you'll
   // need to insert the file names yourself & bump the dep version by hand.
@@ -17,6 +19,7 @@ if (process.env.NODE_ENV !== 'production') {
 app.use(require('./stores/view-state-store'))
 app.use(require('./stores/text-store'))
 app.use(require('./stores/synonym-store'))
+app.use(require('./stores/theme-store'))
 
 // Global handlers
 app.use(require('./common/drop-handler'))
