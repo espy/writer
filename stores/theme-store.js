409e2c4d78f8adae82c7751fe9d188cdb55d111c
Alex Feyerke <espy@users.noreply.github.com>
2018-01-02 15:49:08 +0100

    feat: first-run for themes

--- stores/theme-store.js
+++ stores/theme-store.js
@@ -1,15 +1,42 @@
 const path = window.require('path')
-const fs = window.require('fs')
+const fs = window.require('fs-extra')
 
 module.exports = (state, emitter) => {
   state.themes = []
-  const themePath = path.join(state.paths.extraResources, 'themes')
-  if (themePath) {
-    fs.readdir(themePath, function (err, items) {
-      console.log('available built-in themes: ', items)
+  const internalThemePath = path.join(state.paths.extraResources, 'themes')
+  const userThemePath = path.join(state.paths.userData, 'themes')
+
+  function firstRun () {
+    fs.copy(internalThemePath, userThemePath, (err) => {
       if (err) {
-        console.log('error loading bundled themes:', err)
+        console.log('error copying default themes to the userdata folder:', err)
       }
+      emitter.emit('themes:available', userThemePath)
     })
+  }
+
+  fs.readdir(state.paths.userData, (err, items) => {
+    if (items.indexOf('themes') === -1) {
+      console.log('No themes in user folder')
+      firstRun()
+    } else {
+      emitter.emit('themes:available', userThemePath)
+    }
+    if (err) {
+      console.log('error checking out the userdata folder:', err)
+    }
+  })
+
+  emitter.on('themes:available', loadThemes)
+
+  function loadThemes (path) {
+    if (path) {
+      fs.readdir(path, function (err, items) {
+        console.log('available themes: ', items)
+        if (err) {
+          console.log('error loading user themes:', err)
+        }
+      })
+    }
   }
 }
